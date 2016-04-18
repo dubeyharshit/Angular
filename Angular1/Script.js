21bcec85d607170f814365c67f204894bceaeb70
Harshit Dubey <harsht.235@gmail.com>
2016-04-18 14:54:39 -0700

    Angular

---
+++ Angular1/Script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Harshit on 4/15/2016.
+ */
+///<reference path = "angular.min.js" />
+var myApp= angular.module("myModule",[]).controller("myController",function($scope)
+{
+    var technologies = [
+        {course:"c#",likes:"0",dislikes:"0"},
+        {course:".net",likes:"0",dislikes:"0"}
+    ]
+    $scope.technologies = technologies;
+
+    $scope.likes = function(technology)
+    {
+        technology.likes++;
+    }
+    $scope.dislikes = function(technology)
+    {
+        technology.dislikes++;
+    }
+});
+
